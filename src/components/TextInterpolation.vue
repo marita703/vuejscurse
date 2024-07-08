0602048560644f82d89b0d42feff797be21af2e2
Maria del Mar <chocoinformatics2016@gmail.com>
2024-07-08 09:41:42 +0200

    more text interpolation

--- src/components/TextInterpolation.vue
+++ src/components/TextInterpolation.vue
@@ -2,12 +2,20 @@
 
 <script setup>
 const myMessage = 'Yellow text '
+const myNumber = 100
+function add(x, y) {
+  return x + y
+}
 </script>
 
 <!-- Html -->
 <template>
   <h2>Text Interpolation Example</h2>
   <p>{{ myMessage }}</p>
+  <p>{{ myNumber }}</p>
+  <h3>2 + 2 = {{ 2 + 2 }}</h3>
+  <h4>this is how I interpolate a function</h4>
+  <p>x = 3 ^ y = 2 => x + y = {{ add(3, 2) }}</p>
 </template>
 
 <!-- Css -->
