c5815210af8704f1fa54cd290abf753c6c513b92
Maria del Mar <chocoinformatics2016@gmail.com>
2024-07-08 09:15:52 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<!-- JS -->
+
+<script setup>
+console.log('Hello World')
+</script>
+
+<!-- Html -->
+
+<template>
+  <h1>Hello World</h1>
+</template>
+<!-- Css -->
+
+<style>
+h1 {
+  color: blueviolet;
+}
+</style>
