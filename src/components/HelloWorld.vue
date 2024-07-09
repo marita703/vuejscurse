26aecc9cc85da1c7eeb24c6af205893b18ac1f9f
Maria del Mar <chocoinformatics2016@gmail.com>
2024-07-09 23:42:05 +0200

    v-modal

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,6 +14,7 @@ import MyRefComponent from '@/components/MyRefComponent.vue'
 import ComputedProperties from '@/components/ComputedProperties.vue'
 import ConditionalRenderingComponent from '@/components/ConditionalRenderingComponent.vue'
 import IterationComponent from '@/components/IterationComponent.vue'
+import TwoBindingComponent from '@/components/TwoBindingComponent.vue'
 console.log('Hello World')
 </script>
 
@@ -35,6 +36,7 @@ console.log('Hello World')
     <ComputedProperties />
     <ConditionalRenderingComponent />
     <IterationComponent />
+    <TwoBindingComponent />
   </div>
 </template>
 <!-- Css -->
